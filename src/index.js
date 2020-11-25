4540674468beceb1e8514064cef95b86e16c8e98
Mac <d0hn95@gmail.com>
2020-11-25 17:41:26 +0000

    Version 1.0.0 changes

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import Quill from 'quill'
+const Clipboard = Quill.import('modules/clipboard')
+const Delta = Quill.import('delta')
+
+class VuePlainClipboard extends Clipboard {
+    onPaste (e) {
+        e.preventDefault()
+        const range = this.quill.getSelection()
+        const text = e.clipboardData.getData('text/plain')
+        const delta = new Delta().retain(range.index).delete(range.length).insert(text)
+        const index = text.length + range.index
+        const length = 0
+        this.quill.updateContents(delta, 'silent')
+        this.quill.setSelection(index, length, 'silent')
+        this.quill.scrollIntoView()
+    }
+}
+
+export default VuePlainClipboard
